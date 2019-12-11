e4f275b8c205df18976a9e1d2fefdf7857082be4
Klaus Volmer <kl.volmer@gmx.de>
2019-12-11 17:16:37 +0100

    completed "users" stuff

--- src/main/java/de/couchkiwi/deskControl/endpoint/rest/UsersRestController.java
+++ src/main/java/de/couchkiwi/deskControl/endpoint/rest/UsersRestController.java
@@ -1,26 +1,26 @@
 package de.couchkiwi.deskControl.endpoint.rest;
 
-import de.couchkiwi.deskControl.database.UserRepository;
-import de.couchkiwi.deskControl.database.Users;
+import de.couchkiwi.deskControl.database.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
 public class UsersRestController {
 
     private final UserRepository userRepository;
+    private final DeskControlRepository deskControlRepository;
 
     @Autowired
-    public UsersRestController(UserRepository userRepository) {
+    public UsersRestController(UserRepository userRepository,
+                               DeskControlRepository deskControlRepository) {
         this.userRepository = userRepository;
+        this.deskControlRepository = deskControlRepository;
     }
 
     @PostMapping("/add")
@@ -35,4 +35,44 @@ public class UsersRestController {
             return new ResponseEntity<>("User added!", HttpStatus.OK);
         }
     }
-}
+
+   @PostMapping("/add/DeskUser")
+    public ResponseEntity<String> addNewOfficeSpace(@RequestParam("office") String officeSpaceName,
+                                                    @RequestParam("desknumber") String deskNumber,
+                                                    @RequestParam("username") String userName) {
+
+        Optional<Users> userOptional = userRepository.findByUserName(userName);
+        Optional<Long> optionalLong = deskControlRepository.findByOfficeSpaceName(officeSpaceName);
+
+        if (userOptional.isPresent() && optionalLong.isPresent()) {
+            OfficeSpaces office = deskControlRepository.findById(optionalLong.get()).get();
+
+            Desks userDesk = findDeskByDeskNumber(office.getDesksSet(), deskNumber);
+
+            if (userDesk != null) {
+                userDesk.setAssignedUserId(userOptional.get().getUserId());
+                deskControlRepository.save(office);
+
+                return new ResponseEntity<>("User added to Desk!", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Desk not found in given Office!", HttpStatus.OK);
+            }
+
+        } else {
+            return new ResponseEntity<>("User or Desk not found!", HttpStatus.OK);
+        }
+
+    }
+
+    private Desks findDeskByDeskNumber(Set<Desks> desks, String deskNumber) {
+
+        Desks d = desks.stream()
+                .filter(userDesk -> deskNumber.equals(userDesk.getDeskNumber()))
+                .findFirst()
+                .orElse(null);
+
+        return d;
+
+        }
+
+    }
